5d9a12c43d9ae82eca355d85870c2ca5aca4dacf
Dave <dave.lin21@gmail.com>
2019-07-16 22:18:54 -0400

    Updates

---
+++ precache-manifest.cabf847e009575553ae371cd8e24c030.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7796735d4b0a86fb67f2",
+    "url": "/quote_generator/static/css/main.66bfc20e.chunk.css"
+  },
+  {
+    "revision": "7796735d4b0a86fb67f2",
+    "url": "/quote_generator/static/js/main.7796735d.chunk.js"
+  },
+  {
+    "revision": "56c5555c89a61e34a6dd",
+    "url": "/quote_generator/static/js/1.56c5555c.chunk.js"
+  },
+  {
+    "revision": "bb26fb756a2663fa01a8",
+    "url": "/quote_generator/static/js/runtime~main.bb26fb75.js"
+  },
+  {
+    "revision": "4fad2ab0e9b73652472e47c23c11c7a5",
+    "url": "/quote_generator/index.html"
+  }
+];
